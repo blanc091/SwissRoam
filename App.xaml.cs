960d648cdc19103df10a23f9328f01534588d2ca
blanc091 <mircea.albu91@gmail.com>
2024-11-07 02:43:49 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace SwissRoam.Pages
+{
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage()
+		{
+			//InitializeComponent();
+		}
+
+		private void OnLanguageChanged(object sender, EventArgs e)
+		{
+			var picker = (Picker)sender;
+			var selectedLanguage = (string)picker.SelectedItem;
+			DisplayAlert("Language Changed", $"You selected {selectedLanguage}", "OK");
+		}
+
+		private void OnNotificationsToggled(object sender, ToggledEventArgs e)
+		{
+			bool isNotificationsEnabled = e.Value;
+			DisplayAlert("Notifications", isNotificationsEnabled ? "Enabled" : "Disabled", "OK");
+		}
+
+		private void OnDarkModeToggled(object sender, ToggledEventArgs e)
+		{
+			bool isDarkModeEnabled = e.Value;
+			DisplayAlert("Dark Mode", isDarkModeEnabled ? "Enabled" : "Disabled", "OK");
+		}
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SwissRoam
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			//InitializeComponent();
+		}
+	}
+}
